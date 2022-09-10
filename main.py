8988ca630c75297d1d10de5e1c28eafdb45b852d
Ricardo <ricardo@garagecoders.net>
2022-09-10 09:38:46 -0500

    validations_query

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # FastAPI 
 from fastapi import FastAPI 
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -30,3 +30,13 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Validaciones: Query Parameters 
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50), 
+    age: str = Query(...)
+    ):
+    return {'name': name, 'age': age}
