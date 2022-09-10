2b2b95b1bfc5cebc21ee067da3cb1edb70935bbe
Ricardo <ricardo@garagecoders.net>
2022-09-10 09:17:18 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI 
 from fastapi import FastAPI 
+from fastapi import Body
 
 app = FastAPI()
+
+
+# Models 
+class Person(BaseModel):
+    first_name: str
+    last_name: str 
+    age: int
+    hair_color: Optional[str] = None 
+    is_married: Optional[bool] = None
+
+
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
 
+
+# Request and response body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
